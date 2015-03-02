d2f81919a57796602e0a892879bf41ce27a724c9
kfcho <tomcho724@gmail.com>
2015-03-03 00:46:51 +0800

    new

--- blog/src/base/Post.java
+++ blog/src/base/Post.java
@@ -3,6 +3,47 @@ package base;
 import java.util.Date;
 
 public class Post {
-	private Date date;
+	private Date _date;
+	private String _content;
+	
+	public Post(Date date, String content){
+		_date = date;
+		_content = content;		
+	}
+	
+	public String getContent(){
+		return _content;
+	}
+	
+	public void setContent(String content){
+		_content = content;
+	}
+	
+	public Date getDate(){
+		return _date;
+	}
+	
+	public void setDate(Date date){
+		_date = date;
+	}
+	
+	public String toString(){
+		return "";
+	}
+	
+	public boolean equals(Object o){
+		o.getClass();
+		Post post = (Post) o;
+		return(this.getDate() == post.getDate() && this.getContent() == post.getContent());
+	}
+	
+	public int hashCode(){
+		int hashcode = 0;
+		return hashcode;
+	}
+	
+	public boolean contains(String keyword){
+		
+	}
 
 }
--- blog/src/test/TestObject.java
+++ blog/src/test/TestObject.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.util.Date;
+import base.User;
+import base.FrinedPost;
+
 public class TestObject {
 
 }
